3ff00243fafdcf19db0e5ad671664bc38ec68d76
ahmadmohammad96 <ahmad900mohammad@gmail.com>
2026-07-01 01:11:50 +0000

    fix bug

--- custom_helpdesk/www/helpdesk-kalender.py
+++ custom_helpdesk/www/helpdesk-kalender.py
@@ -1,9 +1,10 @@
 import frappe
+from frappe.sessions import get_csrf_token
 
 
 def get_context(context):
     if frappe.session.user == "Guest":
         frappe.throw("Nicht autorisiert", frappe.PermissionError)
     context.no_cache = 1
-    context.csrf_token = (frappe.session.data.get("csrf_token") or "") if frappe.session.data else ""
+    context.csrf_token = get_csrf_token()
     context.current_user = frappe.session.user
